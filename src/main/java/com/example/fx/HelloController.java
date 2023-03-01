1d02af4b23ad9f0cb1ecb6c587ff1b773ef43106
aleja <ale-jimenez@estudiantec.cr>
2023-03-01 16:00:17 -0600

    Hola

---
+++ src/main/java/com/example/fx/HelloController.java
@@ -0,0 +1,21 @@
+package com.example.fx;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
+
+public class HelloController {
+
+    @FXML
+    private Text name;
+
+    @FXML
+    private TextField setName;
+
+    @FXML
+    protected void newName() {
+
+    }
+
+}
